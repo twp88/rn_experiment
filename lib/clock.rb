48f7e1b2b9ecfe5111a5c6c94d08d63ae8ec5f4a
Tom <tom.w.page@gmail.com>
2018-05-26 14:14:15 +0200

    Job done

--- lib/clock.rb
+++ lib/clock.rb
@@ -2,47 +2,37 @@
 class Clock
   attr_reader :standard_numeral
 
+  LATIN_NUMERALS_ONES = ['I', 'II', 'III', 'IV', 'V',
+                         'VI', 'VII', 'VII', 'IX', ''].freeze
+
+  LATIN_NUMERALS_TENS = ['X', 'XX', 'XXX', 'XL', 'L',
+                         'LX', 'LXX', 'LXXX', 'XC', ''].freeze
+
+  LATIN_NUMERALS_HUNDREDS = ['C', 'CC', 'CCC', 'CD',
+                             'D', 'DC', 'DCC', 'DCCC', 'CM', ''].freeze
+
+  LATIN_NUMERALS_THOUSANDS = ['M', 'MM', 'MMM', 'MMMM', ''].freeze
+
   def initialize(standard_numeral)
     @standard_numeral = standard_numeral
-    @latin_numerals_ones = [
-      'I',
-      'II',
-      'III',
-      'IV',
-      'V',
-      'VI',
-      'VII',
-      'VII',
-      'IX',
-      ''
-    ]
-    @latin_numerals_tens = %w[X XX XXX XXXX]
-    @array = [@latin_numerals_tens, @latin_numerals_ones]
-    # All this for constants
+
+    @array = [LATIN_NUMERALS_ONES,
+              LATIN_NUMERALS_TENS,
+              LATIN_NUMERALS_HUNDREDS,
+              LATIN_NUMERALS_THOUSANDS]
   end
 
   def call
-    if double_digit?(@standard_numeral)
-      parse(@standard_numeral)
-    else
-      convert_ones(@standard_numeral)
-    end
+    parse(@standard_numeral)
   end
 
   protected
 
-  def double_digit?(standard_numeral)
-    standard_numeral.length >= 2 ? true : false
-  end
-
-  def convert_ones(standard_numeral)
-    @latin_numerals_ones[standard_numeral.to_i - 1]
-  end
-
   def parse(standard_numeral)
-    # Different method name required
-    standard_numeral.split('')
-                    .map { |n| @array[standard_numeral.index(n)][n.to_i - 1] }
-                    .join('')
+    out = []
+    standard_numeral.split('').reverse.each_with_index do |v, i|
+      out << @array[i][v.to_i - 1]
+    end
+    out.reverse.join('')
   end
 end
--- spec/clock_spec.rb
+++ spec/clock_spec.rb
@@ -8,6 +8,13 @@ describe Clock do
   let(:subject_twenty) { Clock.new(standard_numeral_20) }
   let(:subject_twenty_one) { Clock.new(standard_numeral_21) }
   let(:subject_thirty_five) { Clock.new(standard_numeral_35) }
+  let(:subject_one_hundred) { Clock.new(standard_numeral_100) }
+  let(:subject_one_hundred_one) { Clock.new(standard_numeral_101) }
+  let(:subject_seven_hundred) { Clock.new(standard_numeral_700) }
+  let(:subject_seven_hundred_fifty_one) { Clock.new(standard_numeral_751) }
+  let(:subject_one_thousand) { Clock.new(standard_numeral_1000) }
+  let(:subject_three_thousand_five_hundred_fifty_one) { Clock.new(standard_numeral_3551) }
+  let(:subject_four_thousand_nine_hundred_ninety_nine) { Clock.new(standard_numeral_4999) }
 
   let(:standard_numeral_3) { '3' }
   let(:standard_numeral_5) { '5' }
@@ -16,6 +23,13 @@ describe Clock do
   let(:standard_numeral_20) { '20' }
   let(:standard_numeral_21) { '21' }
   let(:standard_numeral_35) { '35' }
+  let(:standard_numeral_100) { '100' }
+  let(:standard_numeral_101) { '101' }
+  let(:standard_numeral_700) { '700' }
+  let(:standard_numeral_751) { '751' }
+  let(:standard_numeral_1000) { '1000' }
+  let(:standard_numeral_3551) { '3551' }
+  let(:standard_numeral_4999) { '4999' }
 
   describe 'when initialized' do
     it 'initializes correctly' do
@@ -50,6 +64,35 @@ describe Clock do
 
     it 'returns the numeral correctly converted to XXI' do
       expect(subject_thirty_five.call).to eq 'XXXV'
+    end
+
+    it 'returns the numeral correctly converted to C' do
+      expect(subject_one_hundred.call).to eq 'C'
+    end
+
+    it 'returns the numeral correctly converted to CI' do
+      expect(subject_one_hundred_one.call).to eq 'CI'
+    end
+
+    it 'returns the numeral correctly converted to DCC' do
+      expect(subject_seven_hundred.call).to eq 'DCC'
+    end
+
+    it 'returns the numeral correctly converted to DCCLI' do
+      expect(subject_seven_hundred_fifty_one.call).to eq 'DCCLI'
+    end
+
+    it 'returns the numeral correctly converted to M' do
+      expect(subject_one_thousand.call).to eq 'M'
+    end
+
+    it 'returns the numeral correctly converted to MMMDLI' do
+      expect(subject_three_thousand_five_hundred_fifty_one.call).to eq 'MMMDLI'
+    end
+
+    it 'returns the numeral correctly converted to MMMMCMXCIX' do
+      expect(subject_four_thousand_nine_hundred_ninety_nine.call)
+        .to eq 'MMMMCMXCIX'
     end
   end
 end
